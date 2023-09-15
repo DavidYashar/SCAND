f20b4147c033fa34d71c4fcecb38963f586f2c58
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2023-09-15 08:27:33 +0300

    Add files via upload

---
+++ PHP/insert.php
@@ -0,0 +1,40 @@
+<?php
+
+
+require ('autoLoader.php');
+header('Access-Control-Allow-Origin: http://localhost:8000');
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+
+$conn = mysqli_connect('localhost', 'root', '', 'moving');
+
+$productClasses = [
+  'Book' => Book::class,
+  'DVD' => DVD::class,
+  'Furniture' => Furniture::class,
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$productName = $_POST['type'];
+
+if (array_key_exists($productName, $productClasses)) {
+  $productClass = $productClasses[$productName];
+  $product = new $productClass();
+
+  // Insert data using the determined product object
+  $product->insertData();
+
+
+
+}
+}
+
+
+?>
+
+
+      
+    
+    
+
+   
